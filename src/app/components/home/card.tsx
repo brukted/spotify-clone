5fac06fc51866c1cdc4084a8a92fd31c09d57673
Bruk Tedla <biruk.tedla93@gmail.com>
2023-07-07 16:02:13 +0300

    add highlight on hover

--- src/app/components/home/card.tsx
+++ src/app/components/home/card.tsx
@@ -3,7 +3,7 @@ import { Play } from "../icons";
 
 export default function Card() {
   return (
-    <div className="p-4 bg-neutral-900 w-full rounded-md flex flex-col gap-3 group">
+    <div className="p-4 bg-neutral-900 hover:bg-neutral-800 w-full rounded-md flex flex-col gap-3 group">
       <div className="w-full aspect-square relative">
         <Image
           height={100}
